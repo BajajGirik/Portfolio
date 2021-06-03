f553694463a86fb83da89358a933efe2223470f6
Girik Bajaj <bajajgirik2010@gmail.com>
2021-06-03 17:05:12 +0530

    Added animation to header

--- portfolio/src/Components/Header.js
+++ portfolio/src/Components/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 function Header() {
     return (
         <div className="Header">
-            Welcome to my portfolio
+            <span>Welcome to my portfolio</span>
             <p>This is a portfolio website about <span>Girik Bajaj</span></p>
         </div>
     )
