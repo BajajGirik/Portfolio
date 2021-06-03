7d5d1538055ee64d3f45c68c110b94c2085b24c2
Girik Bajaj <bajajgirik2010@gmail.com>
2021-06-03 23:46:20 +0530

    Changed fonts

--- portfolio/src/Components/Footer.js
+++ portfolio/src/Components/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 function Footer() {
     return (
         <div className="Footer">
-            
+           agfu<br/> agfu<br/> agfu<br/> agfu<br/> agfu<br/> agfu<br/> agfu<br/> agfu<br/> agfu<br/> agfu<br/> 
         </div>
     )
 }
