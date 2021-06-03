fcb9571ce578c697da0028516972a41952b1c207
Girik Bajaj <bajajgirik2010@gmail.com>
2021-06-03 22:16:22 +0530

    Finished with main

--- portfolio/src/Components/Main.js
+++ portfolio/src/Components/Main.js
@@ -26,7 +26,6 @@ function Main() {
                 link="https://bajajgirik.github.io/GoogleHomepage/"
                 desc="This is a clone of Google Home Page in which I pass an action to google.com after you search for something. I tried to give it a very original look. It was my first front-end website."
             />
-            <Project title="WikiClone" image="" />
         </div>
     )
 }
