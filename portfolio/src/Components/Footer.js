c1cb2252e65f22985eeb58e14f968dc83aa5109b
Girik Bajaj <bajajgirik2010@gmail.com>
2021-06-04 13:36:55 +0530

    Styled footer

--- portfolio/src/Components/Footer.js
+++ portfolio/src/Components/Footer.js
@@ -3,7 +3,13 @@ import './Footer.css'
 
 function Footer() {
     return (
-        <div className="Footer">          
+        <div className="Footer">
+            <div className="Footer__head">
+                <h2>Thanks For Visiting My Website</h2>
+                <p>If you want to get in touch with me,</p>
+                <p>Then check out my socials down below</p>
+            </div>
+            
             <div className="Footer__contact">
                 <a className="icon solid fa fa-linkedin-square"></a>
                 <a className="icon solid fa fa-github"></a>
