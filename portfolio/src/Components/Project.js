67f236693d75c4b25da88cb8c0da40b6586c9c44
Girik Bajaj <bajajgirik2010@gmail.com>
2021-06-04 20:22:56 +0530

    Made it responsive for phones

--- portfolio/src/Components/Project.js
+++ portfolio/src/Components/Project.js
@@ -1,8 +1,8 @@
 import "./Project.css"
 
-function Project({ title, desc, image, link, style }) {
+function Project({ title, desc, image, link, styles }) {
     return (
-        <div className="Project" data-aos={style} data-aos-duration="3000">
+        <div className="Project" data-aos={styles} data-aos-duration="3000">
             <a href={link}><img src={image} alt="Img not loaded" /></a>
             <div>
                 <center><h2>{title}</h2></center>
