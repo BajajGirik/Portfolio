4f5c7d45af5e1d12249d60bb0b2e9ca03127ef85
Girik Bajaj <bajajgirik2010@gmail.com>
2021-06-04 17:55:41 +0530

    Added animation to project section

--- portfolio/src/Components/Main.js
+++ portfolio/src/Components/Main.js
@@ -11,18 +11,21 @@ function Main() {
             <center className="Main__head"><h1>Some of my projects</h1></center>
             <Project
                 title="MultiplyIt"
+                style="zoom-in-right"
                 image={MultiplyIt}
                 link="https://bajajgirik.github.io/ReactMultiply/"
                 desc="This project was made for people who struggle doing multiplication problems. This website generates 10 random questions and gives your final score after completion. It has got 4 difficulty modes in which you can practice. Built using React.js"
             />
             <Project
                 title="WikiClone"
+                style="zoom-in-left"
                 image={Wikiclone}
                 link="https://github.com/BajajGirik/WikiClone"
                 desc="This project is a Wikipedia clone built using Django. It covers many features like creating a new article, editing an already existing article, fetching a random article, a dedicated content page. This project uses Sqlite as its database."
             />
             <Project
                 title="GoogleHomepage"
+                style="zoom-in-right"
                 image={GoogleHome}
                 link="https://bajajgirik.github.io/GoogleHomepage/"
                 desc="This is a clone of Google Home Page in which I pass an action to google.com after you search for something. I tried to give it a very original look. It was my first front-end website."
--- portfolio/src/Components/Project.js
+++ portfolio/src/Components/Project.js
@@ -1,10 +1,8 @@
-import React, { useEffect } from 'react'
 import "./Project.css"
 
-function Project({ title, desc, image, link }) {
-
+function Project({ title, desc, image, link, style }) {
     return (
-        <div className="Project">
+        <div className="Project" data-aos={style} data-aos-duration="3000">
             <a href={link}><img src={image} alt="Img not loaded" /></a>
             <div>
                 <center><h2>{title}</h2></center>
