e654d69a5c1893e74250058bf2d96b605ac7e00e
Girik Bajaj <bajajgirik2010@gmail.com>
2021-06-04 23:14:18 +0530

    Added animation to about me page

--- portfolio/src/Components/Skills.js
+++ portfolio/src/Components/Skills.js
@@ -3,7 +3,7 @@ import './Skills.css'
 import Resume from '../Pics/Resume.pdf'
 function Skills() {
     return (
-        <div id="Skills">
+        <div id="Skills" data-aos="flip-down" data-aos-duration="3000">
             <center><h1>ABOUT ME</h1></center>
             <div>
                 <img src="https://png.pngtree.com/png-vector/20190321/ourmid/pngtree-vector-users-icon-png-image_856952.jpg" />
@@ -13,7 +13,7 @@ function Skills() {
                 I am currently looking for an internship that is
                 intellectually rewarding and has flexible work timings.<br /><br />
                 For more details about me, check out my resume.
-                <a className="fa fa-download" href={Resume}>Resume</a>
+                <a className="fa fa-download" href={Resume} target="_blank">Resume</a>
                 </p>    
             </div>
         </div>
