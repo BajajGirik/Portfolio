dee14b76469bec8fedab19e2fe9fb379f5e0790d
Girik Bajaj <bajajgirik2010@gmail.com>
2021-06-04 22:48:29 +0530

    Added more content to about me

--- portfolio/src/Components/Skills.js
+++ portfolio/src/Components/Skills.js
@@ -8,10 +8,13 @@ function Skills() {
             <div>
                 <img src="https://png.pngtree.com/png-vector/20190321/ourmid/pngtree-vector-users-icon-png-image_856952.jpg" />
                 <p>My name is Girik Bajaj. 
-                 I am a Full Stack Web Developer and have experience in ReactJS and Django.
-                 I am currently looking for an internship that is intellectually rewarding and
-                 has flexible working style.   
+                I am a Full Stack Web Developer and am proficient in HTML5, CSS3, Javascript; plus
+                modern libraries and frameworks. I have experience working with <strong>Django</strong> and <strong>ReactJs</strong>.
+                I am currently looking for an internship that is
+                intellectually rewarding and has flexible work timings.<br /><br />
+                For more details about me, refer my resume given below.
                 </p>
+                <a></a>
             </div>
         </div>
     )
