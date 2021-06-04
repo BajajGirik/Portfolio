5b76e73ab3f04c4c99d09b8949d61405720e3d07
Girik Bajaj <bajajgirik2010@gmail.com>
2021-06-04 18:48:29 +0530

    Added smooth scroll using reacts package

--- portfolio/src/Components/Header.js
+++ portfolio/src/Components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Header.css'
+import { Link } from "react-scroll";
 
 function Header() {
     return (
@@ -8,7 +9,11 @@ function Header() {
             <span className="Header__about">I am Girik Bajaj</span>
             <div>
                 <p>Scroll Down to know more about me</p>
-                <a className="icon solid fa fa-angle-double-down" href="#Main"></a>
+                <Link className="icon solid fa fa-angle-double-down"
+                    to="Main"
+                    smooth={true}
+                    duration={1000}>
+                </Link>
             </div>
         </div>
     )
