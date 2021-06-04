743eea33a5a87504b37ce67eaadd07d96d0d2094
Girik Bajaj <bajajgirik2010@gmail.com>
2021-06-04 17:34:15 +0530

    Added AOS animations to project

--- portfolio/src/Components/Project.js
+++ portfolio/src/Components/Project.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./Project.css"
 
 function Project({ title, desc, image, link }) {
+
     return (
         <div className="Project">
             <a href={link}><img src={image} alt="Img not loaded" /></a>
