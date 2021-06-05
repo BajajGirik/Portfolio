781589c5d77352d1038a754ee2616148c43c8909
Girik Bajaj <bajajgirik2010@gmail.com>
2021-06-05 20:14:43 +0530

    Used js to style navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import './App.css';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
@@ -6,8 +7,27 @@ import Navbar from './Components/Navbar';
 import Skills from './Components/Skills';
 
 function App() {
+
+  const scrollmax = (e) => {
+    const head = document.querySelector("#Head");
+    const nav = document.querySelector(".Navbar");
+    if (window.scrollY >= head.offsetHeight - nav.offsetHeight) {
+      nav.style.backgroundColor = "#B15";
+    }
+    else {
+      nav.style.backgroundColor = "transparent";
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', scrollmax);
+    return () => {
+      window.addEventListener('scroll', scrollmax);
+    }
+  }, [])
   return (
     <div className="App">
+      <Navbar />
       <Header />
       <Skills />
       <Main />
--- portfolio/src/Components/Navbar.js
+++ portfolio/src/Components/Navbar.js
@@ -5,30 +5,32 @@ import './Navbar.css'
 function Navbar() {
     return (
         <div className="Navbar">
-           <Link className="Navbar__list"
-                to="Head"
-                smooth={true}
-                duration={1000}>
-            Home    
-            </Link> 
-           <Link className="Navbar__list"
-                to="Skills"
-                smooth={true}
-                duration={1000}>
-            About Me   
-            </Link> 
-            <Link className="Navbar__list"
-                to="Main"
-                smooth={true}
-                duration={1000}>
-            Projects    
-            </Link> 
-            <Link className="Navbar__list"
-                to="ContactMe"
-                smooth={true}
-                duration={1000}>
-            Contact  
-            </Link> 
+            <div className="Navbar__container">    
+                <Link className="Navbar__list"
+                        to="Head"
+                        smooth={true}
+                        duration={1000}>
+                    Home    
+                    </Link> 
+                <Link className="Navbar__list"
+                        to="Skills"
+                        smooth={true}
+                        duration={1000}>
+                    About Me   
+                </Link> 
+                <Link className="Navbar__list"
+                    to="Main"
+                    smooth={true}
+                    duration={1000}>
+                Projects    
+                </Link> 
+                <Link className="Navbar__list"
+                    to="ContactMe"
+                    smooth={true}
+                    duration={1000}>
+                    Contact  
+                </Link>
+            </div>    
         </div>
     )
 }
